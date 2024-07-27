3faf68f3022e4788801f76d25da652e025ab2492
nidhihiwse <hiwse.nid@gmail.com>
2024-07-28 00:01:09 +0900

    refactor

--- components/product_details/Product.tsx
+++ components/product_details/Product.tsx
@@ -44,6 +44,7 @@ const Product: React.FC = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+        
         {/* Left Side - Slider */}
         <div className="md:col-span-3">
           <SliderComponent
